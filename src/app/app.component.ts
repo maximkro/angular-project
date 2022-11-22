6121ee0467c0f46ccaa404180ef27590ac061976
kikilove5me <77271656+kikilove5me@users.noreply.github.com>
2022-11-23 00:41:47 +0100

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-studies';
+  title = 'angular app';
+  product: IProduct[] = data;
 }
